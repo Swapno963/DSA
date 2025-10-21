ee03525063dc1763de2e1e6b739e9ddbbddd79b2
swapno mondol <swapno963@gmail.com>
2025-10-21 10:07:26 +0600

    solved 4 sum problems

---
+++ Sorting/4sum.py
@@ -0,0 +1,28 @@
+nums = [3,2,3,-3,1,0]
+target = 3
+output = []
+f_set = set()
+nums.sort()
+# print(nums)
+for h in range(0, len(nums)-3):
+    for i in range(h+1, len(nums)-2):
+        j, k =  i + 1, len(nums) - 1
+
+        while(j < k):
+            print(h,i,j,k)
+            st = f"{nums[h]}{nums[i]}{nums[j]}{nums[k]}"
+            if st not in f_set:
+                if nums[h] + nums[i] + nums[j] + nums[k] == target:
+                    output.append([nums[h], nums[i], nums[j], nums[k]])
+                    f_set.add(st)
+                    j += 1
+                    k -= 1
+                elif nums[h] + nums[i] + nums[j] + nums[k] > target:
+                    k -= 1
+                elif nums[h] + nums[i] + nums[j] + nums[k] < target:
+                    j += 1
+
+            else:
+                j += 1
+                k -= 1
+print(output, "set is : ",f_set)
