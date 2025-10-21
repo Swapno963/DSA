c0157f35dce2290ad6cd789f1a7502903acb5284
swapno mondol <swapno963@gmail.com>
2025-10-21 10:16:02 +0600

    test files added

--- Sorting/test.py
+++ Sorting/test.py
@@ -1,22 +1,26 @@
 
 # s = "abbda"
 s="aca"
-
+# s = "abbadc"
 def check_palindrome(le,ri):  
-    print(le, ri)  
+    # print(le, ri)  
     while(le<ri):
         if s[le] != s[ri]:
             return False
         le += 1
         ri -= 1
     return True
+
+
 l,r = 0, len(s)-1       
 while(l<r):
     if s[l] != s[r]:
         r_l = check_palindrome(l+1, r)
         r_r = check_palindrome(l, r-1)
         # return r_l or r_r
-        print(f"r_l : {r_l} and r_r : {r_r}")
+        # print(f"r_l : {r_l} and r_r : {r_r}")
+        print(r_l or r_r)
+        break
     l += 1
     r -= 1
 # print(s, "un_match_count is :",un_match_count)
